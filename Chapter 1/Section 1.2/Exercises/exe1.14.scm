230397cf9c207e1a67129fb0624cb389e9fa457e
yth1 <yuancao85@gmail.com>
2018-05-23 22:55:44 +0000

    Finished Exercise 1.4

---
+++ Chapter 1/Section 1.2/Exercises/exe1.14.scm
@@ -0,0 +1,7 @@
+; The order of growth for count-change procedure's time complexity is O(n^2) as
+; n approaches infinity.
+;
+; The order of growth for count-change procedure's space complexity is O(n) as n
+; approaches infinity.
+;
+; n is the amount of money that needs to be changed.
