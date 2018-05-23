03952b9149a6209bf91a71b669f4e0998c507ef4
yth1 <yuancao85@gmail.com>
2018-05-23 23:27:42 +0000

    feat: Corrected exercise 1.14

--- Chapter 1/Section 1.2/Exercises/exe1.14.scm
+++ Chapter 1/Section 1.2/Exercises/exe1.14.scm
@@ -1,7 +1,16 @@
-; The order of growth for count-change procedure's time complexity is O(n^2) as
-; n approaches infinity.
+; The orders of growth in time and space complexities depends on the number of
+; kinds of change there are, as well as the amount of money to be changed.
 ;
-; The order of growth for count-change procedure's space complexity is O(n) as n
-; approaches infinity.
+; If the amount of money is large enough, there will be many layers where the
+; procedure will generate as many recursive calls as there are different kinds
+; of coins.
 ;
-; n is the amount of money that needs to be changed.
+; If the amount is a, to use the same notation as SICP, and let's use d to
+; denote the different kinds of denominations of coins we have, then the time
+; complexity growth is growth is O(a^d).
+;
+; Assuming that this is a single thread process, and there are no unseen
+; multiprocessing optimizations underneath, each new recursive call only need to
+; track the caller that generated. Thus, the amount of space complexity growth
+; is only proportional to the maximum depth of the tree, which is a, since the
+; greatest depth is reached by line where a is changed into pennies.
