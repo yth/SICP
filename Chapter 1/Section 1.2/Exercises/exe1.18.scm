e9595c41600f118ab0ab887674668a999272ec42
yth1 <yuancao85@gmail.com>
2018-05-27 20:04:05 +0000

    feat: Assessed usefulness of exercise 1.18

--- Chapter 1/Section 1.2/Exercises/exe1.18.scm
+++ Chapter 1/Section 1.2/Exercises/exe1.18.scm
@@ -1,3 +1,9 @@
 ; This has the same general solution as 1.16.
 ;
 ; This exercise is skipped at this reading.
+;
+; In essence, this is helps you create an algorithm for integer operations
+; that's O(log(n)). In addition, doubling and halving can use shift
+; operations, which is more efficient.
+;
+; This would be a useful exercise to complete in the future.
