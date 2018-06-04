8febd85dcb567411f9bc115810a89bd9d8e835d0
yth1 <yuancao85@gmail.com>
2018-06-04 21:02:19 +0000

    feat: Finished exercise 1.32 part b; missed previously

--- Chapter 1/Section 1.3/Exercises/exe1.32.scm
+++ Chapter 1/Section 1.3/Exercises/exe1.32.scm
@@ -1,5 +1,6 @@
 ; Generalized accumulate
 
+; iterative version
 (define (accumulate combiner null-value term a next b)
     (if (> a b)
         null-value
@@ -13,8 +14,16 @@
     )
 )
 
+; recursive version
+(define (accumulate-recr combiner null-value term a next b)
+    (if (> a b)
+        null-value
+        (combiner (term a) (accumulate combiner null-value term (next a) next b))
+    )
+)
+
 (define (sum term a next b)
-    (accumulate + 0 term a next b)
+    (accumulate-recr + 0 term a next b)
 )
 
 (define (product term a next b)
