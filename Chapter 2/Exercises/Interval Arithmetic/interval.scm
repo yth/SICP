5faca5cefdc4430a96096a59defff32f1c08921f
yth1 <yuancao85@gmail.com>
2018-06-19 15:27:48 +0000

    feat: Finished exercise 2.7

--- Chapter 2/Exercises/Interval Arithmetic/interval.scm
+++ Chapter 2/Exercises/Interval Arithmetic/interval.scm
@@ -27,4 +27,42 @@
 (display (rp (max-val r1 t1) (max-val r2 t2)))
 |#
 
+(define (add-interval x y)
+    (make-interval (+ (lower-bound x) (lower-bound y))
+                   (+ (upper-bound x) (upper-bound y))
+    )
+)
+
+(define (mul-interval x y)
+    (let 
+        (
+            (p1 (* (lower-bound x) (lower-bound y)))
+            (p2 (* (lower-bound x) (upper-bound y)))
+            (p3 (* (upper-bound x) (lower-bound y)))
+            (p4 (* (upper-bound x) (upper-bound y)))
+        )
+
+        (make-interval (min p1 p2 p3 p4) (max p1 p2 p3 p4))
+    )
+)
+
+(define (div-interval x y)
+    (mul-interval x
+        (make-interval (/ 1.0 (upper-bound y)) (/ 1.0 (lower-bound y)))
+    )
+)
+
+; Exercise 2.7
+
+(define (make-interval a b) (cons a b))
+
+; Clearly there is an issue of order, although the choice doesn't matter as much
+; as having made a choice.
+;
+; In standard notation, (a, b) denote a range from a to b, non-inclusive, b > a.
+; I will adopt a similar notation.
+
+(define (lower-bound x) (car x))
+(define (upper-bound x) (cdr x))
+
 
