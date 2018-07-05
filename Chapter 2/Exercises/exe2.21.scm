b1b56f54e3cdee040723779c3be6501102058a5f
yth1 <yuancao85@gmail.com>
2018-07-05 17:26:04 +0000

    feat: Finished exercise 2.21

---
+++ Chapter 2/Exercises/exe2.21.scm
@@ -0,0 +1,18 @@
+(define (square x) (* x x))
+
+(define (square-list items)
+    (if (null? items)
+        '()
+        (cons (square (car items)) (square-list (cdr items)))
+    )
+)
+
+(newline)
+(display (square-list (list 1 2 3 4)))
+
+(define (square-list items)
+    (map square items)
+)
+
+(newline)
+(display (square-list (list 1 2 3 4)))
