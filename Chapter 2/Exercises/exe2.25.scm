69eddd05b3ea31293310f3bc4b4a789a77866b46
yth1 <yuancao85@gmail.com>
2018-07-11 15:30:12 +0000

    feat: Finished Exercise 2.25

---
+++ Chapter 2/Exercises/exe2.25.scm
@@ -0,0 +1,14 @@
+; Exercise 2.25
+
+(newline)
+(display (car (cdaddr (list 1 3 (list 5 7) 9))))
+(newline)
+(display (caar (list (list 7))))
+(newline)
+(display
+    (cadadr
+        (cadadr
+            (cadadr (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+        )
+    )
+)
