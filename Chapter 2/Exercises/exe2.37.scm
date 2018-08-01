522d15258d5ce9c811904962c802994696249392
yth1 <yuancao85@gmail.com>
2018-08-01 15:48:44 +0000

    feat: Finished exercise 2.37, changed matrix-*-vector to produce a vector instead of a matrix

--- Chapter 2/Exercises/exe2.37.scm
+++ Chapter 2/Exercises/exe2.37.scm
@@ -22,7 +22,7 @@
 
 ; Exercise
 (define (matrix-*-vector m v)
-    (map (lambda (row) (list (dot-product row v))) m)
+    (map (lambda (row) (dot-product row v)) m)
 )
 
 (define m (list (list 1 2 3 4) (list 4 5 6 6) (list 6 7 8 9)))
@@ -39,19 +39,23 @@
 (newline)
 (display (transpose m))
 
-#|
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
 (define (matrix-*-matrix m n)
     (let 
         (
             (cols (transpose n))
         )
 
-        (map <??> m))
+        (map (lambda (row) (matrix-*-vector cols row)) m))
 )
 
 (define i2 (list (list 1 0) (list 0 1)))
 (define ones2 (list (list 1 1) (list 1 1)))
+(define other2 (list (list 1 2) (list 3 4)))
 (newline)
-(display (matrix-*-matrix m n))
-
-|#
+(display (matrix-*-matrix other2 i2))
