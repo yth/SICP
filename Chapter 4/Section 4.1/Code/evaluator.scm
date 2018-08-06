fada5a2b51dd779b10d71bc23f7f1ea20ff6d859
yth1 <yuancao85@gmail.com>
2018-08-06 20:51:22 +0000

    feat: Copied down apply procedure from 4.1

--- Chapter 4/Section 4.1/Code/evaluator.scm
+++ Chapter 4/Section 4.1/Code/evaluator.scm
@@ -23,3 +23,20 @@
     )
 )
 
+(define (apply procedure arguments)
+    (cond
+        ((primitive-procedure? procedure)
+            (apply-primitive-procedure procedure arguments)
+        )
+        ((compound-procedure? procedure)
+            (eval-sequence
+                (procedure-body procedure)
+                (extend-environment
+                    (procedure-parameters procedure)
+                    arguments
+                    (procedure-environments procedure))
+            )
+        )
+        (else (error "Unknown procedure type -- APPLY" procedure))
+    )
+)
