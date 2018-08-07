fa3d201d0f94b4f1159ea6e0f72b397d4d759dd3
yth1 <yuancao85@gmail.com>
2018-08-07 15:51:26 +0000

    feat: Copied eval-if function

--- Chapter 4/Section 4.1/Code/evaluator.scm
+++ Chapter 4/Section 4.1/Code/evaluator.scm
@@ -55,4 +55,11 @@
     )
 )
 
+(define (eval-if exp env)
+    (if (true? (eval (if-predicate exp) env))
+        (eval (if-consequent exp) env)
+        (eval (if-alternative exp) env)
+    )
+)
+
 
