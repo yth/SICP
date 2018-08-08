c0c79d3ec58e71e5839a2e55c73ef88a65db632a
yth1 <yuancao85@gmail.com>
2018-08-08 16:43:19 +0000

    feat: Copied eval-sequence

--- Chapter 4/Section 4.1/Code/evaluator.scm
+++ Chapter 4/Section 4.1/Code/evaluator.scm
@@ -62,4 +62,11 @@
     )
 )
 
+(define (eval-sequences exps env)
+    (cond 
+        ((last-exp? exps) (eval (first-exp exps) env))
+        (else (eval (first-exp exps) env) (eval-sequence (rest-exps exps) env))
+    )
+)
+
 
