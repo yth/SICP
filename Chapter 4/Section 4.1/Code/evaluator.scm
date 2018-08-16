6db8de2bdc8f8c7f1e2954bd15dabd737f6e9214
yth1 <yuancao85@gmail.com>
2018-08-16 16:31:19 +0000

    feat: Copied definition functions

--- Chapter 4/Section 4.1/Code/evaluator.scm
+++ Chapter 4/Section 4.1/Code/evaluator.scm
@@ -119,4 +119,27 @@
 
 (define (assignment-value exp) (caddr exp))
 
+; Definition
+
+(define (definition? exp)
+    (tagged-list? exp 'define)
+)
+
+(define (definition-variable exp)
+    (if (symbol? (cadr exp))
+        (cadr exp)
+        (caadr exp)
+    )
+)
+
+(define (definition-value exp)
+    (if (symbol? (cadr exp))
+        (caddr exp)
+        (make-lambda
+            (cdadr exp)
+            (cddr exp)
+        )
+    )
+)
+
 
