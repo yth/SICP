5ad21c9b4c73c3cb47038f1ee0b99a4abb9cc307
yth1 <yuancao85@gmail.com>
2018-08-17 15:07:53 +0000

    feat: Copied lambda functions

--- Chapter 4/Section 4.1/Code/evaluator.scm
+++ Chapter 4/Section 4.1/Code/evaluator.scm
@@ -142,4 +142,14 @@
     )
 )
 
+; Lambda expressions
+
+(define (lambda? exp) (tagged-list? exp 'lambda))
+(define (lambda-parameters exp) (cadr exp))
+(define (lambda-body exp) (cddr exp))
+
+(define (make-lambda parameters body)
+    (cons 'lambda (cons parameters body))
+)
+
 
