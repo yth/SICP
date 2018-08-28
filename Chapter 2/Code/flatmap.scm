a818ca6ba44237b5656344e2a4f39ff9abfe5547
yth1 <yuancao85@gmail.com>
2018-08-28 17:07:37 +0000

    feat: Fully expanded flatmap example

--- Chapter 2/Code/flatmap.scm
+++ Chapter 2/Code/flatmap.scm
@@ -12,6 +12,17 @@
 ; test
 (newline)
 (display (enumerate-interval 2 7))
+(newline)
+(display (enumerate-interval 0.5 7))
+
+(define (accumulate op initial sequence)
+    (if (null? sequence)
+        initial
+        (op (car sequence)
+            (accumulate op initial (cdr sequence))
+        )
+    )
+)
 
 #|
 (accumulate
@@ -24,10 +35,60 @@
                 (enumerate-interval 1 (- i 1))
             )
         )
-    
-        (enumerate-interval 1 n)
+       (enumerate-interval 1 n)
     )
 )
 |#
 
+(define (flatmap proc seq)
+    (accumulate append nil (map proc seq))
+)
 
+(define (prime-sum? pair)
+    (prime? (+ (car pair) (cadr pair)))
+)
+
+(define (make-pair-sum pair)
+    (list (car pair) (cadr pair) (+ (car pair) (cadr pair)))    
+)
+
+(define (find-divisor n test-divisor)
+    (cond
+        ((> (square test-divisor) n) n)
+        (divides? )
+    )    
+)
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+
+
+(define (prime-sum-pairs n)
+    (map make-pair-sum
+        (filter prime-sum?
+            (flatmap
+                (lambda (i)
+                    (map (lambda (j) (list i j))
+                        (enumerate-interval 1 (- i 1))
+                    )
+                )
+                (enumerate-interval 1 n)
+            )
+        )
+    )
+)
+
+(newline)
+(display (prime-sum-pairs 10))
