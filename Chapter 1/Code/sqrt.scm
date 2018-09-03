3db70c2f2030718f0ee5f3cdad858b13508b6fd4
yth1 <yuancao85@gmail.com>
2018-09-03 17:55:05 +0000

    chore: added all of sqrt example to sqrt.scm

--- Chapter 1/Code/sqrt.scm
+++ Chapter 1/Code/sqrt.scm
@@ -20,3 +20,18 @@
 )
 
 (define (sqrt x) (sqrt-iter 1.0 x))
+
+(define (sqrt-hidden x)
+    (define (good-enough? guess x)
+        (< (abs (- (square guess) x)) tolerance))
+    (define (average x y)
+        (/ (+ x y) 2))
+    (define (improve guess x)
+        (average guess (/ x guess)))
+    (define (sqrt-iter guess x)
+        (if (good-enough? guess x)
+            guess
+            (sqrt-iter (improve guess x) x)))
+
+    (sqrt-hidden 1.0 x)
+)
