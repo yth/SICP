f66d5f0171573cbddf553c27da5d9c5b612d1cca
Yuan Cao <yuancao85@gmail.com>
2018-09-26 13:02:04 -0400

    Added code up to fib as a iterative process

--- Chapter 1/Code/notes.scm
+++ Chapter 1/Code/notes.scm
@@ -55,3 +55,45 @@
 
 (define (sqrt x) (sqrt-iter 1.0 x))
 
+(define (factorial n)
+    (if (= n 1)
+        1
+        (* n (factorial (- n 1)))
+    )
+)
+
+(define (factorial n)
+    (define (fact-iter product counter max-count)
+        (if (> counter max-count)
+            product
+            (fact-iter (* counter product)
+                    (+ counter 1)
+                    (max-count)
+            )
+        )
+    )
+
+    (fac-iter 1 1 n)
+)
+
+(define (fib n)
+    (cond
+        ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))
+              )
+        )
+    )
+)
+
+(define (fib n)
+    (define (fib-iter a b count)
+        (if (= counter 0)
+            b
+            (fib-iter (+ a b) a (- count 1))
+        )
+    )
+
+    (fib-iter 1 0 n)
+)
