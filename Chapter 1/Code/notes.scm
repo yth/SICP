24e115a267f7db8823d92a9f00b50a114dcabefe
yth1 <yuancao85@gmail.com>
2018-09-27 15:46:56 +0000

    Added count change to notes

--- Chapter 1/Code/notes.scm
+++ Chapter 1/Code/notes.scm
@@ -55,3 +55,39 @@
 
 (define (sqrt x) (sqrt-iter 1.0 x))
 
+#| ; Name protection, and lexical scoping
+(define (sqrt x)
+    (define (good-enough? ...))
+    ...
+    (sqrt-iter 1.0 x)
+)
+|#
+
+; Counter Change
+
+(define (count-change amount)
+    (cc amount 5)    
+)
+
+(define (cc amount kinds-of-coins)
+    (cond
+        ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else
+            (+ (cc amount (- kinds-of-coins 1))
+               (cc (- amount (first-denomination kinds-of-coins)) kinds-of-coins)
+            )
+        )
+    )    
+)
+
+(define (first-denomination kinds-of-coins)
+    (cond
+        ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)
+    )    
+)
+
